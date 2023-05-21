24129d57ea2782dda5fddcb0ac356295677b37c0
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-05-21 10:32:08 +1000

    added python lambdas

---
+++ lambdas/CheckFileExtensionLambda.py
@@ -0,0 +1,15 @@
+import urllib.parse
+import os
+import json
+
+
+def lambda_handler(event, context):
+    """Reads event information from EventBridge and extracts and returns filename"""
+    print("Received event: " + json.dumps(event, indent=2))
+    # Get the object from the event and show its content type
+    bucket = event["detail"]["bucket"]["name"]
+    key = urllib.parse.unquote_plus(event["detail"]["object"]["key"], encoding="utf-8")
+    filename, file_extension = os.path.splitext(key)
+    print(f"File extension is: {file_extension}")
+    payload = {"file_extension": file_extension, "bucket": bucket, "key": key}
+    return payload
