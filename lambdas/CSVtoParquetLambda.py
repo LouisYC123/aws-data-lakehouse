b5ed10da76767cf311ecc268f48da6b093153637
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-05-25 18:05:27 +1000

    update lambdas

--- lambdas/CSVtoParquetLambda.py
+++ lambdas/CSVtoParquetLambda.py
@@ -1,6 +1,9 @@
-import awswrangler as wr
-import os
-from datetime import datetime
+from landing_zone_events import (
+    get_source_event_info,
+    create_source_target_s3_keys,
+    create_glue_tables,
+    convert_csv_to_parquet,
+)
 
 
 def lambda_handler(event, context):
@@ -8,54 +11,16 @@ def lambda_handler(event, context):
     We get the s3 bucket name and the object key from event, and set the
     Glue catalog db_name and table_name based on the path of the object that
     was uploaded."""
-
-    # Get the source bucket and object name as passed to the Lambda function
-    bucket = event["bucket"]
-    key = event["key"]
-
-    # Set the Glue Catalog DB and table name based on the last two elements
-    # of the path prior to the file name.
-    key_list = key.split("/")
-    print(f"key_list: {key_list}")
-    db_name = key_list[len(key_list) - 1]
-    print(f"db_name: {db_name}")
-    table_name = datetime.now().strftime("%Y%m%d_%H%M%S")
-    print(f"table_name: {table_name}")
-
-    # print out some debug type information that will be captured in our
-    # lambda function logs.
-    print(f"Bucket: {bucket}")
-    print(f"key: {key}")
-
-    input_path = f"s3://{bucket}/{key}"
-    print(f"Input_Path: {input_path}")
-    output_path = f"s3://{os.environ['clean_zone_bucket_name']}/{db_name}/{table_name}"
-    print(f"Output_Path: {output_path}")
-
-    # read the contents of the CSV file into a pandas DataFrame we
-    input_df = wr.s3.read_csv(input_path)
-    print(f"df shape: {str(input_df.shape)}")
-    current_databases = wr.catalog.databases()
-    # wr.catalog.databases()
-    if db_name not in current_databases.values:
-        print(f"- Database {db_name} does not exist... creating")
-        wr.catalog.create_database(db_name)
-    else:
-        print(f"- Database {db_name} already exists.")
-
-    # use the AWS Data Wrangler library to create a Parquet file
-    print("starting save to parquet")
-
-    result = wr.s3.to_parquet(
-        df=input_df,
-        path=output_path,
-        dataset=True,
-        database=db_name,
-        table=table_name,
-        mode="append",
-    )
+    source_event_info = get_source_event_info(
+        bucket=event["source_bucket"],
+        key=event["source_key"],
+        )
+    s3_paths = create_source_target_s3_keys(source_event_info)
+    create_glue_tables(source_event_info)
+    convert_csv_to_parquet(source_event_info, s3_paths)
     return {
-        "db_name": db_name,
-        "source_bucket":bucket,
-        "source_s3_key":key,
-    }
+        'db_name': event['filename'],
+        'source_s3_key': s3_paths['input_path'],
+        }
+
+
